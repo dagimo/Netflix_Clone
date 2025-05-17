1a31677c5045becc28a151e3e216f7e7b769ab59
Tibebe <2dagimo@gmail.com>
2025-05-17 23:10:11 +0300

    Rows added including videos

--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Row from '../Row/Row'
+import requests from '../../../utils/requests'
 
 function RowList() {
   return (
-    <div>RowList</div>
+    <>
+    <Row 
+        title="NETFLIX ORIGNINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}/>
+    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>
+    <Row title="Action Moview" fetchUrl={requests.fetchActionMovies}/>
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+    <Row title="TV Show" fetchUrl={requests.fetchTvShows}/>
+    <Row title="Documentaries" fetchUrl={requests.fectchDocumentaries}/>
+    </>
   )
 }
 
